1a28e180891ba17d7e21bd5865aca100ea49d516
radl97 <radl97@users.noreply.github.com>
2020-10-02 16:39:48 +0200

    Dirty: commit as it worked

--- receiver.c
+++ receiver.c
@@ -4,17 +4,50 @@
 #include <unistd.h> // usleep(int)
 
 int main() {
-	// TODO
+	int ctr = 0;
+	int RED = 0;
+	int GREEN = 0;
 	while (1) {
-		sched_yield();
 
-		register long long a = probe_timing(shared_memory+0);
+		register long long ref = probe_timing(LINE_NOPE);
+		register long long red = probe_timing(LINE_0);
+		register long long green = probe_timing(LINE_1);
 		//sched_yield();
 		//register long long b = probe_timing(shared_memory+1);
+		if (green > 500) {
+			GREEN++; // PROOF that green is 0
+		}
+		if (red > 500) {
+			RED++;	// PROOF that red is 0
+		}
+		ctr++;
+		if (ctr == 300) {
+			printf("\x1B[31mBLIP%lld\x1B[0m\n", RED);
+			printf("\x1B[32mBLOOP%lld\x1B[0m\n", GREEN);
+			printf("\x1B[33m%lld\x1B[0m\n", red);
+			printf("\x1B[33m%lld\x1B[0m\n", green);
+			printf("\x1B[33m%lld\x1B[0m\n", ref);
 
+			int guess = 0;
+			if (RED < 3) { guess |= 2; }
+			if (GREEN < 10) { guess |= 1; }
+			printf("\x1B[1m%lld\x1B[0m", guess);
+
+			ctr = 0;
+			RED = 0;
+			GREEN = 0;
+		}
+
+/*
 		for (int i = 0; i < a;  i+=50)
 			printf(" ");
-		printf("%lld\n", a);
-		usleep(10000);
+		printf("\x1B[31m%lld\x1B[0m\n", a);
+		for (int i = 0; i < b;  i+=50)
+			printf(" ");
+		printf("\x1B[32m%lld\x1B[0m\n", b);
+		for (int i = 0; i < ref;  i+=50)
+			printf(" ");
+		printf("\x1B[33m%lld\x1B[0m\n", ref);*/
+		usleep(1000);
 	}
 }
--- sender.c
+++ sender.c
@@ -3,14 +3,37 @@
 #include <sched.h> // sched_yield()
 #include <unistd.h> // usleep(int)
 
+uint64_t sum = 0;
+
 INLINE void send(int bit) { // no "bool" in C
-	// TODO
-	maccess(shared_memory+bit);
+	if (bit)
+		sum+=maccess(LINE_0);
+	else
+		sum+=maccess(LINE_1);
 }
 
 int main() {
 	while (1) { // true and false are not present in C
-		send(0);
-		send(1);
+		printf("3\n");
+		for (long long i = 0; i < 10000000000ll; i++) {
+			sum += maccess(LINE_BASE);
+			send(0);
+			send(1);
+		}
+		printf("2\n");
+		for (long long i = 0; i < 20000000000ll; i++) {
+			sum += maccess(LINE_BASE);
+			send(1);
+		}
+		printf("1\n");
+		for (long long i = 0; i < 20000000000ll; i++) {
+			sum += maccess(LINE_BASE);
+			send(0);
+		}
+		printf("0\n");
+		for (long long i = 0; i < 30000000000ll; i++) {
+			sum += maccess(LINE_BASE);
+		}
+		printf("%d\n", sum);
 	}
 }
