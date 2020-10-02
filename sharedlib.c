d42bcd87ec9bcfeb8210917abe509edb23830541
radl97 <radl97@users.noreply.github.com>
2020-10-02 16:39:13 +0200

    Initialize shared mem, define 4 cachelines

--- sharedlib.c
+++ sharedlib.c
@@ -1,2 +1,5 @@
-char shared_buffer[4096] __attribute__((aligned(4096)));
+char shared_buffer[4096] __attribute__((aligned(4096))) = {
+	'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+    'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+};
 char* const shared_memory __attribute__((visibility("default"))) = shared_buffer;
--- sharedlib.h
+++ sharedlib.h
@@ -3,5 +3,9 @@
 
 #include <stdio.h>
 extern char *shared_memory;
+#define LINE_0 (shared_memory + 65)
+#define LINE_1 (shared_memory + 65*5)
+#define LINE_BASE (shared_memory)
+#define LINE_NOPE (shared_memory+65*12)
 
 #endif
